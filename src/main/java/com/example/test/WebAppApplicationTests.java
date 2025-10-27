188fce04f5c481340e7825e2a8baf4bfc4affad7
labuser <labuser@ip-172-31-14-2.ap-south-1.compute.internal>
2025-10-27 14:29:46 +0000

    Added dummy test for Jenkins JUnit report

---
+++ src/main/java/com/example/test/WebAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true, "Application context loaded successfully");
+    }
+}
